bc5c4f65de834544df84c33768f6d7e8823ed8f1
Surja11 <stha.surja123@gmail.com>
2025-05-07 19:23:14 +0545

    Added contact

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField()
+  message= models.TextField()
+
+class Project(models.Model):
+  name = models.CharField(max_length=200)
+  short_description = models.TextField()
+  thumbnail = models.ImageField()
+  tech_used = models.CharField(max_length=500)
+  link = models.CharField(max_length= 500)
+
+  
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import *
+from .forms import *
+from django.http import JsonResponse
 # Create your views here.
+def index(request):
+  form = ContactForm()
+  return render(request, 'app/index.html', {'form': form})
+
+
