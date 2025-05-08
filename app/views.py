add994d57565fd6c5f39e161efb769de0e292994
Surja11 <stha.surja123@gmail.com>
2025-05-08 19:08:50 +0545

    added contact to admin and updated view

--- app/views.py
+++ app/views.py
@@ -4,6 +4,12 @@ from .forms import *
 from django.http import JsonResponse
 # Create your views here.
 def index(request):
+  if request.method == "POST":
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return JsonResponse({"msg": "successs"})
+    
   form = ContactForm()
   return render(request, 'app/index.html', {'form': form})
 
